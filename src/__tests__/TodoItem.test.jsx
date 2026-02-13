ba2651c232f9356565f50c8a1476e7161f8bb2a1
jadesitaa <jadesita.work@GMAIL.COM>
2026-02-13 16:06:34 +0700

    todoitem getby

--- src/__tests__/TodoItem.test.jsx
+++ src/__tests__/TodoItem.test.jsx
@@ -3,33 +3,47 @@ import { describe, it, expect } from 'vitest'
 import TodoItem from '../TodoItem.jsx'
 
 describe('TodoItem', () => {
-  const baseTodo = {
-    id: 1,
-    title: 'Sample Todo',
-    done: false,
-    comments: [],
-  };
-
-
-  it('renders with no comments correctly', () => {
-    render(<TodoItem todo={baseTodo} />);
-    expect(screen.getByText('Sample Todo')).toBeInTheDocument();
-  });
-
-  it('renders with comments correctly', () => {
-    const todoWithComment = {
-      ...baseTodo,
-      comments: [
-        { id: 1, message: 'First comment' },
-        { id: 2, message: 'Another comment' },
-      ]
+    const baseTodo = {
+        id: 1,
+        title: 'Sample Todo',
+        done: false,
+        comments: [],
     };
 
-    render(<TodoItem todo={todoWithComment} />);
 
-    expect(screen.getByText('Sample Todo')).toBeInTheDocument();
+    it('renders with no comments correctly', () => {
+        render(<TodoItem todo={baseTodo} />);
+        expect(screen.getByText('No comments')).toBeInTheDocument();
+    });
 
-    expect(screen.getByText('First comment')).toBeInTheDocument();
-    expect(screen.getByText('Another comment')).toBeInTheDocument();
-  });
+    it('does not show no comments message when it has a comment', () => {
+        const todoWithComment = {
+            ...baseTodo,
+            comments: [
+                { id: 1, message: 'First comment' },
+            ]
+        };
+        render(
+            <TodoItem todo={todoWithComment} />
+        );
+        expect(screen.queryByText('No comments')).not.toBeInTheDocument();
+    });
+
+    it('renders with comments correctly', () => {
+        const todoWithComment = {
+            ...baseTodo,
+            comments: [
+                { id: 1, message: 'First comment' },
+                { id: 2, message: 'Another comment' },
+            ]
+        };
+
+        render(<TodoItem todo={todoWithComment} />);
+
+        expect(screen.getByText('Sample Todo')).toBeInTheDocument();
+
+        expect(screen.getByText('First comment')).toBeInTheDocument();
+        expect(screen.getByText('Another comment')).toBeInTheDocument();
+        expect(screen.getByText(/2/)).toBeInTheDocument();
+    });
 });
