2031118e2872a8f47177d487498abfbf33eaf53a
jadesitaa <jadesita.work@GMAIL.COM>
2026-02-14 22:56:05 +0700

    add todoItem newf deletetodo

--- src/__tests__/TodoItem.test.jsx
+++ src/__tests__/TodoItem.test.jsx
@@ -1,6 +1,7 @@
-import { render, screen } from '@testing-library/react'
-import { describe, it, expect } from 'vitest'
+import { render, screen, fireEvent } from '@testing-library/react'
+import { describe, it, expect, vi } from 'vitest'
 import TodoItem from '../TodoItem.jsx'
+import userEvent from '@testing-library/user-event'
 
 describe('TodoItem', () => {
     const baseTodo = {
@@ -45,5 +46,51 @@ describe('TodoItem', () => {
         expect(screen.getByText('First comment')).toBeInTheDocument();
         expect(screen.getByText('Another comment')).toBeInTheDocument();
         expect(screen.getByText(/2/)).toBeInTheDocument();
+    });
+
+    it('makes callback to toggleDone when Toggle button is clicked', () => {
+    const onToggleDone = vi.fn();
+    render(
+      <TodoItem 
+       todo={baseTodo} 
+       toggleDone={onToggleDone} />
+    );
+    const button = screen.getByRole('button', { name: /toggle/i });
+    button.click();
+    expect(onToggleDone).toHaveBeenCalledWith(baseTodo.id);
+  });
+
+    it('makes callback to deleteTodo when delete button is clicked', () => {
+        const onDeleteTodo = vi.fn();
+
+        render(
+        <TodoItem 
+            todo={baseTodo} 
+            deleteTodo={onDeleteTodo} 
+        />
+        );
+
+        const button = screen.getByRole('button', { name: '❌' }); 
+        
+        button.click();
+        expect(onDeleteTodo).toHaveBeenCalledWith(baseTodo.id);
+    });
+
+    it('makes callback to addNewComment when a new comment is added', async () => {
+        const onAddNewComment = vi.fn();
+        render(
+        <TodoItem 
+            todo={baseTodo} 
+            addNewComment={onAddNewComment} 
+        />
+        );
+
+        const input = screen.getByRole('textbox');
+        await userEvent.type(input, 'New comment');
+
+        const button = screen.getByRole('button', { name: /add comment/i });
+        fireEvent.click(button);
+
+        expect(onAddNewComment).toHaveBeenCalledWith(baseTodo.id, 'New comment');
     });
 });
